ae7a5a3d1af3c82eaf40e696490aceccfc8182f6
haenanim121@gmail.com <haenanim121@gmail.com>
2023-05-27 17:03:29 +0900

    feat : Carousel 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,11 @@ import logo from './logo.svg';
 import Carousel from './components/Carousel/Carousel';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Carousel></Carousel>
+    </div>
+  );
 }
 
 export default App;
