7520da077ce74c4dcc326cc97b425246bb777319
haenanim121@gmail.com <haenanim121@gmail.com>
2023-05-30 23:55:36 +0900

    feat : 엘리먼트 삭제기능 구현

--- src/components/CustomAlert/CustomAlert.tsx
+++ src/components/CustomAlert/CustomAlert.tsx
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 interface props {
   children: any;
 }
 export default function CustomAlert({ children }: props) {
   const [alerts, setAlerts] = useState<any>([]);
+  const $alertElement = useRef<HTMLDivElement>(null);
+  const [timeoutId, setTimeoutId] = useState<any>(null);
+
   function addAlert(content: any) {
-    setAlerts([content, ...alerts]);
+    const index: any = alerts.length;
+    const a = [
+      ...alerts,
+      <div id={index} key={index} ref={$alertElement}>
+        {content}
+      </div>,
+    ];
+    setAlerts(a);
+    settingInterval();
+    // setAlerts([content, ...alerts]);
+  }
+  function deleteInterval() {
+    $alertElement.current?.remove();
+    clearInterval(timeoutId);
+  }
+  function settingInterval() {
+    setTimeoutId(setInterval(deleteInterval, 2000));
   }
   return (
     <div>
-      <div>
-        {alerts.map((element: any) => {
-          return <div>{element}</div>;
-        })}
-      </div>
+      <div>{alerts}</div>
       <button
         onClick={() => {
           {
