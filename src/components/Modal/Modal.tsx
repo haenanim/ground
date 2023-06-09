ed8b030ab7875da5b632c389c59008afa48e4929
haenanim121@gmail.com <haenanim121@gmail.com>
2023-06-10 02:39:01 +0900

    fix : 헤더 css 수정

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -12,11 +12,22 @@ export default function Modal({ children }: props) {
       {children}
       {showModal ? (
         <div>
-          <div className="modal-background">배경</div>
-          <div>
-            <div>헤더</div>
-            <div>바디</div>
-            <div>푸터</div>
+          <div className="modal-background">
+            <div className="modal-body">
+              <div className="modal-header">
+                <div className="modal-title">해 보시겠습니까</div>
+                <div
+                  className="modal-close-btn"
+                  onClick={() => {
+                    setShowModal(false);
+                  }}
+                >
+                  X
+                </div>
+              </div>
+              <div className="modal-content">바디</div>
+              <div className="modal-footer">푸터</div>
+            </div>
           </div>
         </div>
       ) : (
