ec442281d1efa06fd6fa89e3fed81d15c066496c
haenanim121@gmail.com <haenanim121@gmail.com>
2023-06-09 23:15:47 +0900

    feat : 모달 css 생성

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { createContext } from 'react';
+import React, { useState } from 'react';
+import './Modal.scoped.scss';
 
 interface props {
   children: any;
 }
 export default function Modal({ children }: props) {
+  const [showModal, setShowModal] = useState(true);
+
   return (
-    <div>
+    <React.Fragment>
       {children}
-      <button>배경</button>
-      <div>
-        <div>헤더</div>
-        <div>바디</div>
-        <div>푸터</div>
-      </div>
-    </div>
+      {showModal ? (
+        <div>
+          <div className="modal-background">배경</div>
+          <div>
+            <div>헤더</div>
+            <div>바디</div>
+            <div>푸터</div>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </React.Fragment>
   );
 }
